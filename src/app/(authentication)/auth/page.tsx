3a7d9075292d6f0ac5e170b30b58e663bbdab530
Montassar Hajri <montassarhajri@outlook.com>
2024-10-15 11:20:14 +0100

    Implement social login functionality :) and enhance authentication flow and test it

--- src/app/(authentication)/auth/page.tsx
+++ src/app/(authentication)/auth/page.tsx
@@ -11,11 +11,17 @@ import { FcGoogle } from "react-icons/fc";
 import { MdOutlineAutoAwesome } from "react-icons/md";
 import { RxGithubLogo } from "react-icons/rx";
 import { z } from "zod";
+import { useState } from "react";
+import { Provider } from "@supabase/supabase-js";
+import { sbBrowserClient } from "@/sb/SBClient";
 
 const defaultAuthPage = () => {
 
+    const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+
     const formSchema = z.object({
-        email: z.string().email().min(4, { message: 'Email must be above 4 characters' }),
+        email: z.string().email({message:"put a valid email"}).min(4, { message: 'Email must be above 4 characters' }),
     });
 
     const form = useForm<z.infer<typeof formSchema>>({
@@ -27,6 +33,15 @@ const defaultAuthPage = () => {
 
     async function onSubmit(values: z.infer<typeof formSchema>) {
         console.log(values);
+    }
+
+    async function socialLogin(provider:Provider) {
+        setIsAuthenticating(true);
+        await sbBrowserClient.auth.signInWithOAuth({
+            provider,
+            options: { redirectTo: `${location.origin}/auth/callback`, },
+        });
+        setIsAuthenticating(false);
     }
 
     return (
@@ -52,7 +67,8 @@ const defaultAuthPage = () => {
                 />
 
                 <div className="flex flex-col space-y-4 mb-6">
-                    <Button variant="outline" className="flex items-center justify-center space-x-2 border border-gray-300 hover:bg-gray-50 transition duration-200">
+                    <Button disabled={isAuthenticating}  variant="outline" className="flex items-center justify-center space-x-2 border border-gray-300 hover:bg-gray-50 transition duration-200"
+                    onClick={() => socialLogin("google")}>
                         <FcGoogle size={24} />
                         <TextBlock
                             className="text-lg"
@@ -60,7 +76,8 @@ const defaultAuthPage = () => {
                             variant="p"
                         />
                     </Button>
-                    <Button variant="outline" className="flex items-center justify-center space-x-2 border border-gray-300 hover:bg-gray-50 transition duration-200">
+                    <Button  disabled={isAuthenticating} variant="outline" className="flex items-center justify-center space-x-2 border border-gray-300 hover:bg-gray-50 transition duration-200"
+                    onClick={() => socialLogin("github")}>
                         <RxGithubLogo size={24} />
                         <TextBlock
                             className="text-lg"
@@ -78,7 +95,7 @@ const defaultAuthPage = () => {
 
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)}>
-                            <fieldset>
+                            <fieldset disabled={isAuthenticating}>
                                 <FormField
                                     control={form.control}
                                     name='email'
--- src/sb/SBClient.ts
+++ src/sb/SBClient.ts
@@ -1,6 +1,6 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export const sbBrowserClient = createBrowserClient(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_ANON_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 );
